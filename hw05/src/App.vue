cb74e91035047f345202cc95651972e9fc0dd1eb
Denis.Karev <goofy.adun@gmail.com>
2023-11-30 17:21:04 +0300

    hw05 p2

---
+++ hw05/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <ProductsList />
+  </div>
+</template>
+
+<script>
+import ProductsList from './components/ProductsList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ProductsList
+  }
+}
+</script>
+
+<style lang="scss"></style>
